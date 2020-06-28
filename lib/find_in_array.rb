48085c338cfaeadd2e3e09b2d260cd5b5aa62823
Brad Beggs <brad.beggs@gmail.com>
2020-06-28 19:49:19 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,7 @@
 def find_element_index(array, value_to_find)
   index = 0
-  while index < array.size do
+  for array.size.times do
+  #while index < array.size do
     puts array[index] == value_to_find
     index += 1
   end
