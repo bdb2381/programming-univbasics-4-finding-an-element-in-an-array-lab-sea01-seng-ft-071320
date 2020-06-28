2da23112f2b562812f9eea545fdc01599f8b88a7
Brad Beggs <brad.beggs@gmail.com>
2020-06-28 19:53:17 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,6 +4,7 @@ def find_element_index(array, value_to_find)
     puts array[index] == value_to_find
     if array[index] == value_to_find
       return array[index]
+    else
     index += 1
   end
 
