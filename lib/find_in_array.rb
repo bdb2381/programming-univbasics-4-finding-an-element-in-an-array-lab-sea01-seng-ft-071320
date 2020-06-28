5d03ffeeadba84a8cc96bf493553003434fba500
Brad Beggs <brad.beggs@gmail.com>
2020-06-28 19:56:17 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,10 +2,10 @@ def find_element_index(array, value_to_find)
   index = 0
   array.size.times do
     puts array[index] == value_to_find
-    if array[index] == value_to_find
+    if array[index] == value_to_find{
       puts "test code"
       return array[index]
-
+    }
     else
     index += 1
   end
