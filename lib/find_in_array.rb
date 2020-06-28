948b152179fe2d9c66471a958d2e3f90010f614d
Brad Beggs <brad.beggs@gmail.com>
2020-06-28 20:12:59 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,7 @@
 def find_element_index(array, value_to_find)
   index = 0
-  puts value_to_find
-  puts array
-  array.size.times do
-    #puts array[index] == value_to_find
-    if array[index] == value_to_find
+    array.size.times do
+      if array[index] == value_to_find
       puts "test code"
       #location = array[index]
       location = array.index(value_to_find)
@@ -13,5 +10,5 @@ def find_element_index(array, value_to_find)
     end
     index += 1
   end
-  return 
+  return
 end
